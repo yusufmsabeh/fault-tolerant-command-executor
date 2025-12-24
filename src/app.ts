5e29290de36c8fe1a9c9743a36dc57b0aabf511e
yusufmsabeh <yussef.msabeh@gmail.com>
2025-12-24 13:44:44 +0200

    feat: post command now is working

--- src/app.ts
+++ src/app.ts
@@ -1,9 +1,10 @@
 import express from "express";
 import { router as testRouter } from "./routes/test.routes";
+import { router as commandsRouter } from "./routes/commands.routes";
 import { requestLogger } from "./middleware/request-logger.middleware";
 
 export const app = express();
 
 app.use(express.json());
 app.use(requestLogger);
-app.use("/api", testRouter);
+app.use("/api", [testRouter,commandsRouter]);
---
+++ src/services/command.service.ts
@@ -0,0 +1,16 @@
+import { Command } from "../models/command.model";
+import { v4 as uuid } from "uuid";
+
+export class CommandService {
+  static async createCommand(type: string, payload: object) {
+    const command = await Command.create({
+      id: uuid(),
+      type,
+      payload,
+      status: "PENDING",
+      attempt: 0,
+    });
+
+    return command;
+  }
+}
